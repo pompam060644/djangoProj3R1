7ab7ff1b2539a95545799573a861dd467ab39ccd
pp060644 <patcharaporn.du@rmuti.ac.th>
2023-01-23 13:56:10 +0700

    third

--- ProfileApp/views.py
+++ ProfileApp/views.py
@@ -15,3 +15,25 @@ def rloemodel(request):
     return render(request, 'rloe.html')
 def etc(request):
     return render(request, 'etc.html')
+def mydata(request):
+    name = "นางสาวพัชราพร ดวงจิตร"
+    stdid = "65342310015-1"
+    address = "329 ม.11 บ้านนาจาน ต.หนองแปน อ.มัญจาคีรี จ.ขอนแก่น 40160"
+    gender = "หญิง"
+    weigth = "48"
+    heigth = "168"
+    colors = "สีชมพู"
+    food = "อาหารประเภทเส้น"
+    job = "นักศึกษา"
+    myproduct = [["รองเท้า adidas", "1,499", "images/141.jpg"],
+                 ["รองเท้า nike", "1,599", "images/142.jpg"],
+                 ["รองเท้า converse", "1,299", "images/143.jpg"],
+                 ["รองเท้า Keds", "1,099", "images/144.jpg"],
+                 ["รองเท้า New balance", "1,399", "images/145.jpg"],
+                 ["รองเท้า Onitsuka Tiger", "1,199", "images/146.jpg"],
+                 ["รองเท้า Reebok", "1,099", "images/147.jpg"],
+                 ["รองเท้า Lacoste", "1,399", "images/148.jpg"],
+                 ]
+    return render(request, 'showmydata.html', {'name':name, 'stdid':stdid, 'address':address, 'gender':gender,
+                                          'weigth':weigth, 'heigth':heigth, 'colors':colors, 'food':food,
+                                          'job':job, 'myproduct':myproduct})
