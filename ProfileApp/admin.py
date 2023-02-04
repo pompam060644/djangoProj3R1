4a22b928a51afa1237158a11c38825a612fbefbf
pp060644 <patcharaporn.du@rmuti.ac.th>
2023-02-04 23:11:12 +0700

    third

--- ProfileApp/admin.py
+++ ProfileApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Customer)
+admin.site.register(Goods)
+admin.site.register(GoodsCategory)
+admin.site.register(Order)
+admin.site.register(OderDetails)
--- ProfileApp/models.py
+++ ProfileApp/models.py
@@ -40,3 +40,50 @@ class product():
         return self.__discount
     def getNet(self):
         return self.__net
+#lab12
+class GoodsCategory(models.Model):
+    cid = models.CharField(max_length=6, primary_key=True, default="")
+    gc_name = models.CharField(max_length=50, default="")
+    desc = models.CharField(max_length=200, default="")
+    def __str__(self):
+        return str(self.cid)+ ":" + self.gc_name + ":" + self.desc
+
+class Goods(models.Model):
+    g_category = models.ForeignKey(GoodsCategory, on_delete=models.CASCADE, default=None)
+    gid = models.CharField(max_length=6, primary_key=True, default="")
+    g_name = models.CharField(max_length=50, default="")
+    brand = models.CharField(max_length=50, default="")
+    g_models = models.CharField(max_length=50, default="")
+    price = models.FloatField(default=0.00)
+    Net = models.IntegerField(default=0)
+    product = models.CharField(max_length=200, default="")
+    def __str__(self):
+        return str(self.gid) + ":" + self.g_name + ":" + str(self.price)
+
+class Customer(models.Model):
+    cid = models.CharField(max_length=6, primary_key=True, default="")
+    name = models.CharField(max_length=50, default="")
+    surname = models.CharField(max_length=50, default="")
+    address = models.CharField(max_length=200, default="")
+    tel = models.CharField(max_length=10, default="")
+    gender = models.CharField(max_length=1, default="")
+    carreer = models.CharField(max_length=50, default="")
+    password = models.CharField(max_length=50, default="")
+    def __str__(self):
+        return str(self.cid) + ":" + self.name + ":" + self.surname + ":" + self.tel
+
+class Order(models.Model):
+    oid = models.CharField(max_length=6, primary_key=True, default="")
+    date = models.DateField(auto_now_add=True, blank=True)
+    cid = models.ForeignKey(Customer, on_delete=models.CASCADE, default=None)
+    status = models.CharField(max_length=50, default="")
+    def __str__(self):
+        return str(self.oid) + ":" + str(self.date) + ":" + str(self.cid)
+class  OderDetails(models.Model):
+    did = models.CharField(max_length=6, primary_key=True, default="")
+    oid = models.ForeignKey(Order, on_delete=models.CASCADE, default=None)
+    gid = models.ForeignKey(Goods, on_delete=models.CASCADE, default=None)
+    price = models.FloatField(default= 0.00)
+    quantity = models.IntegerField(default=0)
+    def __str__(self):
+        return str(self.did) + ":" + str(self.oid) + ":" + str(self.gid) + ":" + str(self.price)
